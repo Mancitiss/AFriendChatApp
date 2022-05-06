552be361af185ce4a31be5486d49f5244d2d6b2f
Mancitiss <vovuongthanhtu@gmail.com>
2022-05-06 21:49:37 +0700

    Update Program.java

--- A Friend Java/AFriendJava/src/main/java/com/mycompany/afriendjava/Program.java
+++ A Friend Java/AFriendJava/src/main/java/com/mycompany/afriendjava/Program.java
@@ -1,9 +1,55 @@
 package com.mycompany.afriendjava;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.swing.JFrame;
 
 public class Program {
+
+    public static JFrame mainform;
+    public static int[] thisversion = {3, 2, 1, 2};
     public static void main(String[] args) {
-        Login login = new Login();
-        login.setVisible(true);
+        try{
+            boolean newv = false;
+            try{
+                URL url = new URL("http://mancitiss.duckdns.org/index.htm");
+                String data = null;
+                try(BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()))){
+                   data = reader.readLine();
+                }
+                System.out.println(data);
+                String[] newestversion = data.split("\\.");
+                int[] newestversionint = new int[newestversion.length];
+                for(int i = 0; i < newestversion.length; i++){
+                    newestversionint[i] = Integer.parseInt(newestversion[i]);
+                }
+                System.out.println("This version: " + thisversion[0] + "." + thisversion[1] + "." + thisversion[2] + "." + thisversion[3]);
+                System.out.println("Newest version: " + newestversionint[0] + "." + newestversionint[1] + "." + newestversionint[2] + "." + newestversionint[3]);
+                for(int i = 0; i < thisversion.length; i++){
+                    if (i>=4) break;
+                    if(thisversion[i] < newestversionint[i]){
+                        newv = true;
+                        break;
+                    }
+                    else if (newestversionint[i] == thisversion[i]){
+                        continue;
+                    }
+                    else break;
+                }
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+            if (newv){
+                (new Login()).setVisible(true);
+            } else {
+                (new Login()).setVisible(true);
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
