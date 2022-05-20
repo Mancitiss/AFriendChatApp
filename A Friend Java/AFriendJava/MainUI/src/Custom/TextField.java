0b1ef3c785ebec0a41f51f772cf3b4770991a251
Phạm Nhật Minh <minhphamhklovetory@gmail.com>
2022-05-20 16:51:12 +0700

    Upload MainUI(unfinish)

---
+++ A Friend Java/AFriendJava/MainUI/src/Custom/TextField.java
@@ -0,0 +1,30 @@
+package Custom;
+import javax.swing.JTextField;
+public class TextField extends JTextField{
+	private TextFieldUI textUI;
+
+    public TextField() {
+        textUI = new TextFieldUI(this);
+        setUI(textUI);
+    }
+
+    public void addItemSuggestion(String text) {
+        textUI.getItems().add(text);
+    }
+
+    public void removeItemSuggestion(String text) {
+        textUI.getItems().remove(text);
+    }
+
+    public void clearItemSuggestion() {
+        textUI.getItems().clear();
+    }
+
+    public void setRound(int round) {
+        textUI.setRound(round);
+    }
+
+    public int getRound() {
+        return textUI.getRound();
+    }
+}
