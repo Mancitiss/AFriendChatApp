7a1906fa079618c3dbd05aa9d4c3ed734e0c91e1
Mancitiss <vovuongthanhtu@gmail.com>
2023-08-20 18:52:36 +0700

    division
    move client and server to their own repository
    this commit remove client from this repo

--- A Friend Java/AFriendJava/src/main/java/Custom/EventSwitchSelected.java
+++
@@ -1,6 +0,0 @@
-package custom;
-
-public interface EventSwitchSelected {
-
-    public void onSelected(boolean selected);
-}
--- A Friend Java/AFriendJava/src/main/java/com/mycompany/afriendjava/BufferedImage.java
+++
@@ -1,5 +0,0 @@
-package com.mycompany.afriendjava;
-
-public class BufferedImage {
-
-}
